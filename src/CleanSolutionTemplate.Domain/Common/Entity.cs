7a0909e1e444f5d2231822e161833ad45019d715
[Bachi] - José Pedro Baccifava <10673163+bachi3@users.noreply.github.com>
2022-09-18 00:13:25 -0300

    Improved Entity Class (#4)
    * Improved Entity Class

--- src/CleanSolutionTemplate.Domain/Common/Entity.cs
+++ src/CleanSolutionTemplate.Domain/Common/Entity.cs
@@ -6,27 +6,15 @@ public abstract class Entity
 {
     private readonly List<DomainEvent> _domainEvents = new();
 
-    protected Entity(Guid id)
-    {
-        this.Id = id;
-    }
+    protected Entity(Guid id) => this.Id = id;
 
     public Guid Id { get; private set; }
 
-    [NotMapped] public IEnumerable<DomainEvent> DomainEvents => this._domainEvents.AsReadOnly();
+    [NotMapped] public IReadOnlyList<DomainEvent> DomainEvents => this._domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(DomainEvent domainEvent)
-    {
-        this._domainEvents.Add(domainEvent);
-    }
+    public void AddDomainEvent(DomainEvent domainEvent) => this._domainEvents.Add(domainEvent);
 
-    public void RemoveDomainEvent(DomainEvent domainEvent)
-    {
-        this._domainEvents.Remove(domainEvent);
-    }
+    public void RemoveDomainEvent(DomainEvent domainEvent) => this._domainEvents.Remove(domainEvent);
 
-    public void ClearDomainEvents()
-    {
-        this._domainEvents.Clear();
-    }
+    public void ClearDomainEvents() => this._domainEvents.Clear();
 }
