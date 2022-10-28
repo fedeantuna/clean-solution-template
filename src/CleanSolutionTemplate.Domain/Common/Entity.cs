9c2e305b425f835cd73082d1ec1c72e54bf318d4
Federico Antuña <federicoantuna@hotmail.com>
2022-10-28 17:38:55 -0300

    Improved Entity Class

--- src/CleanSolutionTemplate.Domain/Common/Entity.cs
+++ src/CleanSolutionTemplate.Domain/Common/Entity.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CleanSolutionTemplate.Domain.Common;
 
@@ -6,15 +7,23 @@ public abstract class Entity
 {
     private readonly List<DomainEvent> _domainEvents = new();
 
-    protected Entity(Guid id) => this.Id = id;
+    protected Entity(Guid id) =>
+        this.Id = id;
 
-    public Guid Id { get; private set; }
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+    public Guid Id { get; init; }
 
-    [NotMapped] public IReadOnlyList<DomainEvent> DomainEvents => this._domainEvents.AsReadOnly();
+    [NotMapped]
+    [SuppressMessage("ReSharper", "ReturnTypeCanBeEnumerable.Global")]
+    public IReadOnlyCollection<DomainEvent> DomainEvents =>
+        this._domainEvents.AsReadOnly();
 
-    public void AddDomainEvent(DomainEvent domainEvent) => this._domainEvents.Add(domainEvent);
+    public void AddDomainEvent(DomainEvent domainEvent) =>
+        this._domainEvents.Add(domainEvent);
 
-    public void RemoveDomainEvent(DomainEvent domainEvent) => this._domainEvents.Remove(domainEvent);
+    public void RemoveDomainEvent(DomainEvent domainEvent) =>
+        this._domainEvents.Remove(domainEvent);
 
-    public void ClearDomainEvents() => this._domainEvents.Clear();
+    public void ClearDomainEvents() =>
+        this._domainEvents.Clear();
 }
